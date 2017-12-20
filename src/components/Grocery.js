25f55976a0cf74c102b6b6f2f008a6e6e26355fc
Brianna <briannakincart@gmail.com>
2017-12-20 15:46:00 -0500

    all-code

---
+++ src/components/Grocery.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Grocery = props => {
+  let groceryName = `${props.name} ${props.selectedText}`;
+
+  return(
+      <li onClick={props.handleClick}>{groceryName}</li>
+  )
+}
+
+export default Grocery;
